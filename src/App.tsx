1538541c28cefec96196a307313e5d48adab0617
Dugi <dugisurdulli@gmail.com>
2024-09-27 23:47:23 -0600

    make sidebar contents sticky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Main } from "./sections/Main";
 
 export const App = () => (
   <div className="w-full flex justify-center my-0 md:my-12 print:my-0">
-    <div className="flex flex-col md:flex-row min-w-96 max-w-screen-lg gap-12 bg-white bg-opacity-70 md:rounded-lg overflow-hidden shadow-xl print:shadow-none">
+    <div className="flex flex-col md:flex-row min-w-96 max-w-screen-lg gap-12 bg-white bg-opacity-70 md:rounded-lg shadow-xl print:shadow-none">
       <Sidebar />
       <Main />
     </div>
